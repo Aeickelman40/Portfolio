97e488609e312ac148aa06e6a9bb9275fba13089
Alex Eickelman <57731927+Aeickelman40@users.noreply.github.com>
2023-06-16 18:27:54 -0700

    Update Hero page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@
 import React, { useReducer } from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
+import Image from "next/image";
 
 type Props = {};
 export default function Hero({}: Props) {
@@ -11,18 +12,29 @@ export default function Hero({}: Props) {
   const [text, count] = useTypewriter({
     words: [
       "Hi, I'm Alex",
-      "Software Developer",
-      "Growth-oriented.tsx",
       "<ConstantLearner />",
+      "Soup Dumpling Enthusiast",
+      "Growth-oriented.tsx",
     ],
     loop: true,
     delaySpeed: 2000,
   });
+
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
+      <Image
+        src="/../public/profile-photo.png"
+        width={500}
+        height={500}
+        alt="Picture of myself"
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+      />
+      <h2 className="text-sm uppercase text-gray-500 tracking-[15px]">
+        Software Engineer
+      </h2>
+      <h1 className="text-5xl lg:text-6xl font-semibold scroll-px-10">
+        <span className="mr-3">{text}</span>
         <Cursor cursorColor="#F7AB0A" />
       </h1>
     </div>

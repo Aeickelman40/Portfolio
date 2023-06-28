0a72972468392f377d64583824e80202e482b6b8
Alex Eickelman <57731927+Aeickelman40@users.noreply.github.com>
2023-06-27 18:32:00 -0700

    Finish Projects, add initial ContactMe

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -20,12 +20,37 @@ function Projects({}: Props) {
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
         {projects.map((project, i) => {
           return (
-            <div className="w-screen flex-shrink-0 snap-center flex flex-col spac-y-5 items-center justify-center p-20 md:p-44 h-screen">
-              <img src="https://picsum.photos/id/237/200/300" alt="" />
-              <div>
-                <h4>
-                  Case Study {i + 1} of {projects.length}: UPS clone
+            <div
+              key={i}
+              className="w-screen flex-shrink-0 snap-center flex flex-col spac-y-5 items-center justify-center p-20 md:p-44 h-screen"
+            >
+              <motion.img
+                initial={{
+                  y: -300,
+                }}
+                transition={{
+                  duration: 1.2,
+                }}
+                whileInView={{
+                  opacity: 1,
+                  y: 0,
+                }}
+                viewport={{
+                  once: true,
+                }}
+                src="https://picsum.photos/id/237/200/300"
+                alt=""
+              />
+              <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+                <h4 className="text-4xl font-semibold text-center">
+                  <span className="underline decoration-[#F7AB0A]/50">
+                    Case Study {i + 1} of {projects.length}:
+                  </span>{" "}
+                  UPS clone
                 </h4>
+                <p className="text-lg text-center md:text-left">
+                  Example project description
+                </p>
               </div>
             </div>
           );

75f40289ed883b09a0465aceb0472611353100e5
Alex Eickelman <57731927+Aeickelman40@users.noreply.github.com>
2023-06-27 19:22:57 -0700

    Add heroicons package

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,9 +1,41 @@
 import React from "react";
+import { PhoneIcon } from "@heroicons/react/24/solid";
 
 type Props = {};
 
 function ContactMe({}: Props) {
-  return <div>ContactMe</div>;
+  return (
+    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Contact
+      </h3>
+      <div className="flex flex-col space-y-10">
+        <h4 className="text-4xl font-semibold text-center">
+          I have got just what you need.{" "}
+          <span className="underline decoration-[#F7AB0A]/50">Lets talk</span>
+        </h4>
+        <div>
+          <PhoneIcon className="h-6 w-6 text-blue-500" />
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ContactMe;
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  strokeWidth={1.5}
+  stroke="currentColor"
+  className="w-6 h-6"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+  />
+</svg>;

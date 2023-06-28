782d33fc582a79d214de1860497d8d8aa44e9b5f
Alex Eickelman <57731927+Aeickelman40@users.noreply.github.com>
2023-06-27 21:41:25 -0700

    Add scrollbar styling

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,11 @@ import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import ContactMe from "@/components/ContactMe";
 
+// TODO: Remove horizontal scroll, make cards draggable, update images, update text
+
 export default function Home({}: NextPage) {
   return (
-    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Header />
       <section id="hero" className="snap-start">
         <Hero />

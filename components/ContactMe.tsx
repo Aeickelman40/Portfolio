c09b07cef83fc3a7d3d45bff1d0c2e89149a5cc3
Alex Eickelman <57731927+Aeickelman40@users.noreply.github.com>
2023-06-27 20:43:13 -0700

    Initial contact setup

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PhoneIcon } from "@heroicons/react/24/solid";
+import { EnvelopeIcon, MapPinIcon, PhoneIcon } from "@heroicons/react/24/solid";
 
 type Props = {};
 
@@ -14,28 +14,32 @@ function ContactMe({}: Props) {
           I have got just what you need.{" "}
           <span className="underline decoration-[#F7AB0A]/50">Lets talk</span>
         </h4>
-        <div>
-          <PhoneIcon className="h-6 w-6 text-blue-500" />
-          <p></p>
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="h-7 w-7 text-[#F7AB0A] animate-pulse" />
+            <p className="text-2xl">970 309 4095</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="h-7 w-7 text-[#F7AB0A] animate-pulse" />
+            <p className="text-2xl">aeickelman40@gmail.com</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <MapPinIcon className="h-7 w-7 text-[#F7AB0A] animate-pulse" />
+            <p className="text-2xl">1010 Railroad Ave.</p>
+          </div>
         </div>
+        <form className="flex flex-col space-y-2 w-fit mx-auto">
+          <div className="flex space-x-2">
+            <input className="contactInput" type="text" />
+            <input className="contactInput" type="text" />
+          </div>
+          <input className="contactInput" type="text" />
+          <textarea className="contactInput" />
+          <button>Submit</button>
+        </form>
       </div>
     </div>
   );
 }
 
 export default ContactMe;
-
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  strokeWidth={1.5}
-  stroke="currentColor"
-  className="w-6 h-6"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
-  />
-</svg>;

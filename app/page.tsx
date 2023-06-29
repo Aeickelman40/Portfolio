5b341e4013cd075b3c6d2e495d6115f19380a37b
Alex Eickelman <57731927+Aeickelman40@users.noreply.github.com>
2023-06-28 20:19:03 -0700

    Remove type from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import type { NextPage } from "next";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
@@ -9,7 +8,7 @@ import Projects from "@/components/Projects";
 import ContactMe from "@/components/ContactMe";
 import Link from "next/link";
 
-export default function Home({}: NextPage) {
+export default function Home() {
   return (
     <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Header />

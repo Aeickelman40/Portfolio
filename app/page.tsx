5ce4bba2b2d0d36de4cbf9760fcc8d0ac4998fe7
Alex Eickelman <57731927+Aeickelman40@users.noreply.github.com>
2023-06-28 18:19:07 -0700

    Add footer link

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import ContactMe from "@/components/ContactMe";
+import Link from "next/link";
 
 export default function Home({}: NextPage) {
   return (
@@ -35,6 +36,17 @@ export default function Home({}: NextPage) {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <img
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0"
+              src="https://picsum.photos/id/237/200/300"
+              alt=""
+            />
+          </div>
+        </footer>
+      </Link>
     </main>
   );
 }

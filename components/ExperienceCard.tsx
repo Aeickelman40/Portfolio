52e7e4114981cb4a37fe22833f962ca62a09c72d
Alex Eickelman <57731927+Aeickelman40@users.noreply.github.com>
2023-07-03 19:35:10 -0700

    ExperienceCard desktop styling

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -18,7 +18,7 @@ const ExperienceCard = ({
   summaryArrary,
 }: Props) => {
   return (
-    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#4d4b4b] p-10 hover:opacity-100 opacity-40 cursor-pointer transistion-opacity duration-200 overflow-hidden">
+    <article className="flex flex-col rounded-lg items-center flex-shrink-0 w-[500px] md:w-[600px] xl:w-[1100px] snap-center bg-[#4d4b4b] p-5 hover:opacity-100 opacity-40 cursor-pointer transistion-opacity duration-200 overflow-hidden mt-8">
       <motion.img
         initial={{
           y: -100,
@@ -34,38 +34,15 @@ const ExperienceCard = ({
         viewport={{
           once: true,
         }}
-        className="w-32 h-32 rounded-full xl:w-[150px] xl:h-[150px] object-cover object-start"
+        className="w-32 h-32 rounded-full xl:w-[150px] xl:h-[150px] object-cover object-start my-6"
         src={imgSrc}
         alt=""
       />
       <div className="px-0 md:px-10">
-        <h4 className="text-3xl font-light">{title}</h4>
-        <p className="font-bold text-2xl mt-1">{company}</p>
-        <div className="flex space-x-2 my-2">
-          <Image
-            src="/../public/profile-photo.png"
-            height={200}
-            width={200}
-            alt=""
-            className="h-10 w-10 rounded-full"
-          />
-          <Image
-            src="/../public/profile-photo.png"
-            height={200}
-            width={200}
-            alt=""
-            className="h-10 w-10 rounded-full"
-          />
-          <Image
-            src="/../public/profile-photo.png"
-            height={200}
-            width={200}
-            alt=""
-            className="h-10 w-10 rounded-full"
-          />
-        </div>
+        <h4 className="text-2xl font-light">{title}</h4>
+        <p className="font-bold text-xl mt-1">{company}</p>
         <p className="uppercase py-5 text-gray-300">{dateRange}</p>
-        <ul className="list-disc space-y-4 ml-5 text-lg">
+        <ul className="list-disc space-y-4 ml-5 text-md">
           {summaryArrary.map((summaryPoint: string) => {
             return <li key={summaryPoint}>{summaryPoint}</li>;
           })}
---
+++ shared/imageUrls.ts
@@ -0,0 +1,8 @@
+export const intevityUrl =
+  "https://media.licdn.com/dms/image/C560BAQFPuFCSF06gRQ/company-logo_200_200/0/1553198011945?e=1695859200&v=beta&t=RDJgRkm10kILAskdAsKrpzYQ2hV5BbV9uz2oPprJa18";
+
+export const kordspaceUrl =
+  "https://media.licdn.com/dms/image/C560BAQG7k-D5q4AtrQ/company-logo_200_200/0/1603909023525?e=1695859200&v=beta&t=qm7Ck5iBX5igALLEYvjdMsxonIx86EU1x5SMZhl_O0w";
+
+export const turingUrl =
+  "https://media.licdn.com/dms/image/C4E0BAQET8fJiAvWTDA/company-logo_200_200/0/1519911325543?e=1695859200&v=beta&t=mU6LcqNP-aMDSQIOpsJ81dNoq5_xFH93L5YDn-XSX4U";

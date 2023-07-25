834cc9276475627796b7a1c88b525b9359737d37
Alex Eickelman <57731927+Aeickelman40@users.noreply.github.com>
2023-07-25 13:57:26 -0700

    Update skills images for mobile

--- TODO.md
+++ TODO.md
@@ -6,7 +6,6 @@ TODO:
 - mobile styling
 - update routing
 - pop up modal for skills w/ explination of experience
-- change projects page for coming soon, prompt to reach out for building something together
 - Add resume link (about page?)
 - Host pdf resume file for url to make a clickable link
 - Move all images to public folder, no urls
--- components/Skill.tsx
+++ components/Skill.tsx
@@ -10,6 +10,11 @@ type Props = {
 function Skill({ directionLeft, logoUrl, name }: Props) {
   return (
     <div className="group relative flex cursor-pointer">
+      <img
+        src={logoUrl}
+        alt=""
+        className="md:hidden rounded-full border border-gray-500 w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out object-contain"
+      />
       <motion.img
         initial={{
           x: directionLeft ? -200 : 200,
@@ -19,7 +24,7 @@ function Skill({ directionLeft, logoUrl, name }: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         src={logoUrl}
         alt=""
-        className="rounded-full border border-gray-500 w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out object-contain"
+        className="hidden md:block rounded-full border border-gray-500 w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out object-contain"
       />
       <div className="absolute opacity-0 group-hover:opacity-80 transistion duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
         <div className="flex items-center justify-center h-full">

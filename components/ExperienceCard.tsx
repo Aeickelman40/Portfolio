98a52da87b2e95fb645f891f88b5d09c75847000
Alex Eickelman <57731927+Aeickelman40@users.noreply.github.com>
2024-08-03 08:50:06 -0700

    Adjust mobile styling for experience and project cards

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -17,14 +17,16 @@ const ExperienceCard = ({
   summaryArrary,
 }: Props) => {
   return (
-    <article className="flex flex-col rounded-lg items-center flex-shrink-0 w-[300px] md:w-[600px] xl:w-[1100px] snap-center bg-[#4d4b4b] p-5 opacity-100 cursor-pointer transistion-opacity duration-200 overflow-hidden mt-20 md:mt-8 hover:border-[#F7AB0A]/40 hover:border h-[670px]">
-      <Image
-        className="w-16 h-16 xl:w-[200px] xl:h-[170px] rounded-lg object-cover object-start my-1 md:my-6"
-        src={imgSrc}
-        alt=""
-        width={200}
-        height={170}
-      />
+    <article className="flex flex-col rounded-lg flex-shrink-0 w-[85vw] md:w-[600px] xl:w-[1100px] snap-center bg-[#4d4b4b] p-5 opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden mt-20 md:mt-8 hover:border-[#F7AB0A]/40 hover:border h-[80vh] md:h-[670px]">
+      <div className="flex justify-center my-1 md:my-6">
+        <Image
+          className="w-16 h-16 xl:w-[200px] xl:h-[170px] rounded-lg object-cover object-center"
+          src={imgSrc}
+          alt=""
+          width={200}
+          height={170}
+        />
+      </div>
       <div className="px-0 md:px-10">
         <h4 className="text-xl md:text-2xl font-light text-center">{title}</h4>
         <p className="font-bold text-xl md:mt-1 text-center">{company}</p>
@@ -32,9 +34,9 @@ const ExperienceCard = ({
           {dateRange}
         </p>
         <ul className="list-disc space-y-2 md:space-y-4 ml-5 text-sm md:text-md text-left">
-          {summaryArrary.map((summaryPoint: string) => {
-            return <li key={summaryPoint}>{summaryPoint}</li>;
-          })}
+          {summaryArrary.map((summaryPoint: string) => (
+            <li key={summaryPoint}>{summaryPoint}</li>
+          ))}
         </ul>
       </div>
     </article>
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -23,48 +23,47 @@ const ProjectCard = ({
   images,
 }: Props) => {
   return (
-    <article className="flex flex-col rounded-lg flex-shrink-0 w-[300px] md:w-[600px] xl:w-[1100px] snap-center bg-[#4d4b4b] p-5 opacity-100 cursor-pointer transistion-opacity duration-200 overflow-hidden mt-20 md:mt-8 hover:border-[#F7AB0A]/40 hover:border h-[670px]">
+    <article className="flex flex-col rounded-lg flex-shrink-0 w-[85vw] md:w-[600px] xl:w-[1100px] snap-center bg-[#4d4b4b] p-5 opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden mt-20 md:mt-8 hover:border-[#F7AB0A]/40 hover:border h-[80vh] md:h-[670px]">
       <div className="px-0 md:px-10">
-        <h4 className="text-xl md:text-2xl font-light">{title}</h4>
-        {!disabled && (
-          <SocialIcon url={url} fgColor="gray" bgColor="transparent" />
-        )}
-        <SocialIcon url={githubUrl} fgColor="gray" bgColor="transparent" />
-        <div className="flex flex-row space-x-10">
-          <div className="">
-            <p className="font-bold text-xl mb-4 md:mt-1 text-left">
-              Tech Stack
-            </p>
+        <h4 className="text-xl md:text-2xl font-light text-center">{title}</h4>
+        <div className="flex justify-center space-x-2 mt-2">
+          {!disabled && (
+            <SocialIcon url={url} fgColor="gray" bgColor="transparent" />
+          )}
+          <SocialIcon url={githubUrl} fgColor="gray" bgColor="transparent" />
+        </div>
+        <div className="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-10 mt-5">
+          <div>
+            <p className="font-bold text-xl mb-4 text-left">Tech Stack</p>
             <ul className="list-disc space-y-2 ml-5 text-xs md:text-lg text-left">
-              {techStacks.map((tech: string) => {
-                return <li key={tech}>{tech}</li>;
-              })}
+              {techStacks.map((tech: string) => (
+                <li key={tech}>{tech}</li>
+              ))}
             </ul>
           </div>
           <div className="flex-1">
-            <p className="font-bold text-xl mb-4 md:mt-1 text-left">
-              Key Features
-            </p>
-            <ul className="list-disc space-y-1 ml-5 text-xs text-left md:text-[15px] md:space-y-3 ">
-              {features.map((feature: string) => {
-                return <li key={feature}>{feature}</li>;
-              })}
+            <p className="font-bold text-xl mb-4 text-left">Key Features</p>
+            <ul className="list-disc space-y-1 ml-5 text-xs md:text-[15px] md:space-y-3 text-left">
+              {features.map((feature: string) => (
+                <li key={feature}>{feature}</li>
+              ))}
             </ul>
           </div>
-          <Carousel className="flex-1 self-center hidden lg:inline-block lg:align-middle">
+          <Carousel className="flex-1 self-center hidden md:flex md:items-center lg:align-middle">
             <CarouselContent>
-              {images.map((image: any) => {
-                return (
-                  <CarouselItem key={image.id}>
-                    <Image
-                      src={image.src}
-                      width={image.width}
-                      height={image.height}
-                      alt={`Image ${image.id}`}
-                    />
-                  </CarouselItem>
-                );
-              })}
+              {images.map((image: any) => (
+                <CarouselItem
+                  key={image.id}
+                  className="flex justify-center items-center h-full"
+                >
+                  <Image
+                    src={image.src}
+                    width={image.width}
+                    height={image.height}
+                    alt={`Image ${image.id}`}
+                  />
+                </CarouselItem>
+              ))}
             </CarouselContent>
           </Carousel>
         </div>
